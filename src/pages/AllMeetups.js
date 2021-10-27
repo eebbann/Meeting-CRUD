73d30cefa0367dd1f28d6f3e6916f0b3b7c4eac9
eebann <ebanemmanuel@live.com>
2021-10-27 05:22:59 -0500

    fetching data from firebase

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import MeetupList from '../components/meetups/MeetupList';
-	const DUMMY_DATA = [
-		{
-			id: 'm1',
-			title: 'This is a first meetup',
-			image:
-				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-			address: 'Meetupstreet 5, 12345 Meetup City',
-			description:
-				'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-		},
-		{
-			id: 'm2',
-			title: 'This is a second meetup',
-			image:
-				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-			address: 'Meetupstreet 5, 12345 Meetup City',
-			description:
-				'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-		},
-	];
+ 
  
 function AllMeetups() {
+	 	fetch ('').then(response => { return response.json()})
+	
+	
 	return (
-		<div>
+	<section>
 
 			<h1 className='header'> Meet ups	</h1>
 			   
 			<MeetupList meetups={DUMMY_DATA}/>
 				  
-		</div>
+		</section>
 	)
 }
 
